3496dc15ea15438f66004663158b7dc1dd2dccad
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-12-07 17:55:13 +0300

    added validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,9 @@
 class Client < ApplicationRecord
     has_secure_password
 
+    validates :username, presence: true, uniqueness: true
+    validates :phone_number, presence: true, length: { is: 10 }
+
     has_many :appointments
     has_many :artists, through: :appointments
 end
